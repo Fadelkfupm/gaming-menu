0863a4d65ff82b19c0bac4816ddbdac038b54700
Fadelkfupm <s202183090@kfupm.edu.sa>
2023-02-09 14:54:01 +0300

    rock_paper_scissors code is done

--- rock_paper_scissor/rock_paper_scissor.py
+++ rock_paper_scissor/rock_paper_scissor.py
@@ -1,2 +1,35 @@
+import random
 def play_rock_paper_scissors():
-    pass
+    computer_choice = random.randint(1,3)
+    print("Select your action : \n"
+    "[1]: Rock \n"
+    "[2]: Paper \n"
+    "[3]: Scissor \n")
+    user_choice = int(input())
+    if user_choice == computer_choice:
+        print("Computer choose " + options(computer_choice) + ", It's a tie.")
+    elif user_choice == 1:
+        if computer_choice == 2:
+            print("Computer choose "+ options(computer_choice) +", You lose.")
+        elif computer_choice == 3:
+            print("Computer choose "+ options(computer_choice) +", You win.")
+    elif user_choice == 2:
+        if computer_choice == 3:
+            print("Computer choose "+ options(computer_choice) +", You lose.")
+        elif computer_choice == 1:
+            print("Computer choose "+ options(computer_choice) +", You win.")
+
+    elif user_choice == 3:
+        if computer_choice == 1:
+            print("Computer choose "+ options(computer_choice) +", You lose.")
+        elif computer_choice == 2:
+            print("Computer choose "+ options(computer_choice) +", You win.")
+
+def options (x):
+    if x == 1:
+        return("Rock")
+    elif x == 2:
+        return("Paper")
+    else :
+        return("Scissor")
+
