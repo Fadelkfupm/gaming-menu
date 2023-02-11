2c4953839a8d718b120b0c77271a0914e3ad1a0a
Fadelkfupm <s202183090@kfupm.edu.sa>
2023-02-11 20:52:49 +0300

    Added the code for each file

--- number_guessing_game/number_guessing_game.py
+++ number_guessing_game/number_guessing_game.py
@@ -1,2 +1,21 @@
-def play_number_guessing_game():
-    pass
+import random
+ def play_number_guessing_game():
+     print("--------- Number guessing ---------")
+     computer_choice = random.randint(1,10)
+     attempts = 3
+     print("Guess the number (Between 1-10)")
+     user_choice = int(input())
+     while(user_choice != computer_choice and attempts != 0):
+         print("Incorrect ):")
+         attempts = attempts - 1
+         if attempts == 0:
+             break
+         print("You have " + str(attempts) + " attempts left.")
+         user_choice = int(input())
+     if user_choice == computer_choice:
+         print("Correct, Well done (:")
+
+
+
+
+     print("--------- Number guessing ---------")
